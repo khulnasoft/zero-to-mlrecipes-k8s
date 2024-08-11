bd8a6d4331d6fcc65bb36285bc113b4efb6220ba
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-08-11 06:02:02 +0000

    update

--- tools/templates/lint-and-validate.py
+++ tools/templates/lint-and-validate.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python3
 """
 Lints and validates the chart's template files and their rendered output without
-any cluster interaction. For this script to function, you must install yamllint.
+any cluster interaction. For this script to function, you must install yamllint
+and kubeval.
 
-USAGE:
+- https://github.com/adrienverge/yamllint
 
-  tools/templates/lint-and-validate.py
+pip install yamllint
 
-DEPENDENCIES:
+- https://github.com/garethr/kubeval
 
-yamllint: https://github.com/adrienverge/yamllint
-
-  pip install yamllint
+LATEST=curl --silent "https://api.github.com/repos/garethr/kubeval/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
+wget https://github.com/garethr/kubeval/releases/download/$LATEST/kubeval-linux-amd64.tar.gz
+tar xf kubeval-darwin-amd64.tar.gz
+mv kubeval /usr/local/bin
 """
 
-import argparse
+
 import os
+import sys
+import argparse
+import glob
 import pipes
 import subprocess
-import sys
 
 os.chdir(os.path.dirname(sys.argv[0]))
-
 
 def check_call(cmd, **kwargs):
     """Run a subcommand and exit if it fails"""
@@ -30,85 +33,69 @@ def check_call(cmd, **kwargs):
     except subprocess.CalledProcessError as e:
         print(
             "`{}` exited with status {}".format(
-                " ".join(map(pipes.quote, cmd)),
+                ' '.join(map(pipes.quote, cmd)),
                 e.returncode,
             ),
             file=sys.stderr,
         )
         sys.exit(e.returncode)
 
-
-def lint(yamllint_config, values, output_dir, strict, debug):
-    """Calls `helm lint`, `helm template`, and `yamllint`."""
+def lint(yamllint_config, values, kubernetes_version, output_dir, debug):
+    """Calls `helm lint`, `helm template`, `yamllint` and `kubeval`."""
 
     print("### Clearing output directory")
-    check_call(["mkdir", "-p", output_dir])
-    check_call(["rm", "-rf", f"{output_dir}/*"])
+    check_call([
+        'mkdir', '-p', output_dir,
+    ])
+    check_call([
+        'rm', '-rf', output_dir + '/*',
+    ])
 
     print("### Linting started")
-    print("### 1/3 - helm lint: lint helm templates")
-    helm_lint_cmd = ["helm", "lint", "../../jupyterhub", f"--values={values}"]
-    if strict:
-        helm_lint_cmd.append("--strict")
-    if debug:
-        helm_lint_cmd.append("--debug")
-    check_call(helm_lint_cmd)
-
-    print("### 2/3 - helm template: generate kubernetes resources")
-    helm_template_cmd = [
-        "helm",
-        "template",
-        "../../jupyterhub",
-        f"--values={values}",
-        f"--output-dir={output_dir}",
+    print("### 1/4 - helm lint")
+    helm_lint_cmd = [
+        'helm', 'lint', '../../jupyterhub',
+        '--values', values,
     ]
     if debug:
-        helm_template_cmd.append("--debug")
+        helm_lint_cmd.append('--debug')
+    check_call(helm_lint_cmd)
+
+    print("### 2/4 - helm template")
+    helm_template_cmd = [
+        'helm', 'template', '../../jupyterhub',
+        '--values', values,
+        '--output-dir', output_dir
+    ]
+    if debug:
+        helm_template_cmd.append('--debug')
     check_call(helm_template_cmd)
 
-    print("### 3/3 - yamllint: yaml lint generated kubernetes resources")
-    check_call(["yamllint", "-c", yamllint_config, output_dir])
+    print("### 3/4 - yamllint")
+    check_call([
+        'yamllint', '-c', yamllint_config, output_dir
+    ])
+
+    print("### 4/4 - kubeval")
+    for filename in glob.iglob(output_dir + '/**/*.yaml', recursive=True):
+        check_call([
+            'kubeval', filename,
+            '--kubernetes-version', kubernetes_version,
+            '--strict',
+        ])
 
     print()
-    print(
-        "### Linting and validation of helm templates and generated kubernetes resources OK!"
-    )
+    print("### Linting and validation of templates finished: All good!")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
-    argparser.add_argument(
-        "--debug",
-        action="store_true",
-        help="Run helm lint and helm template with the --debug flag",
-    )
-    argparser.add_argument(
-        "--strict",
-        action="store_true",
-        help="Run helm lint with the --strict flag",
-    )
-    argparser.add_argument(
-        "--values",
-        default="lint-and-validate-values.yaml",
-        help="Specify Helm values in a YAML file (can specify multiple)",
-    )
-    argparser.add_argument(
-        "--output-dir",
-        default="rendered-templates",
-        help="Output directory for the rendered templates. Warning: content in this will be wiped.",
-    )
-    argparser.add_argument(
-        "--yamllint-config",
-        default="yamllint-config.yaml",
-        help="Specify a yamllint config",
-    )
+    argparser.add_argument('--debug', action='store_true', help='Run helm lint and helm template with the --debug flag')
+    argparser.add_argument('--values', default='lint-and-validate-values.yaml', help='Specify Helm values in a YAML file (can specify multiple)')
+    argparser.add_argument('--kubernetes-version', default='1.11.0', help='Version of Kubernetes to validate against')
+    argparser.add_argument('--output-dir', default='rendered-templates', help='Output directory for the rendered templates. Warning: content in this will be wiped.')
+    argparser.add_argument('--yamllint-config', default='yamllint-config.yaml', help='Specify a yamllint config')
 
     args = argparser.parse_args()
 
-    lint(
-        args.yamllint_config,
-        args.values,
-        args.output_dir,
-        args.strict,
-        args.debug,
-    )
+    lint(args.yamllint_config, args.values, args.kubernetes_version, args.output_dir, args.debug)
